92c20b68bd642dfc200ac2b35428731e2c9ba278
Maryam Abkar <mankara@ncsu.edu>
2024-05-23 17:48:48 -0400

    added maybe like 3 registers, is it correct syntax? will check later

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+
+
+struct Chip8 {
+
+  VxRegisters : vec<u8>,
+  IRegister : u16,  //used to store memory address, usually only the 12 rightmost bits
+                    //are used
+  VFRegister:u16, // is it u16? check. used as flags by some instructions
+  
+
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }
