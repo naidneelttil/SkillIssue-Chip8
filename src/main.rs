917dde2414daf79215306c52dd4591f480cd4bf3
Maryam Abkar <mankara@ncsu.edu>
2024-05-23 19:29:39 -0400

    putting in some of the info of the other registers

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,18 @@
 
 struct Chip8 {
 
-  VxRegisters : vec<u8>,
+  VxRegisters : vec<u8>,  // there needs to be 16 of these
   IRegister : u16,  //used to store memory address, usually only the 12 rightmost bits
                     //are used
   VFRegister:u16, // is it u16? check. used as flags by some instructions
+  TimeRegister:u8,
+  DelayRegister:u8,
   
+  ProgramCounter:u16,
+  StackPointer:u8,
+
+  Stack: vec<u16>, //there needs to be 16 of these
+
 
 }
 
