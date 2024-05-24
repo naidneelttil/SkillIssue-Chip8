362bd75716099bfd021eb7f7eda63a85ef069ff7
Maryam Abkar <mankara@ncsu.edu>
2024-05-24 14:56:37 -0400

    got myself an in browser lsp, so I can fix my code as im writing it

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
-
-
 struct Chip8 {
+    memory: [u8; 4096], // there has to be 4KB aka 4096 bytes of ram
+    vx_reg: [u8; 16],   // there needs to be 16 of these
+    i_reg: u16, // used to store memory address, usually only the 12 rightmost bits//are used
+    vf_reg: u16, // is it u16? check. used as flags by some instructions
+    time_reg: u8,
+    delay_reg: u8,
 
-  VxRegisters : vec<u8>,  // there needs to be 16 of these
-  IRegister : u16,  //used to store memory address, usually only the 12 rightmost bits
-                    //are used
-  VFRegister:u16, // is it u16? check. used as flags by some instructions
-  TimeRegister:u8,
-  DelayRegister:u8,
-  
-  ProgramCounter:u16,
-  StackPointer:u8,
-
-  Stack: vec<u16>, //there needs to be 16 of these
-
-
+    pc: u16,          // program counter
+    stack_ptr: u8,    // stack pointer
+    stack: [u16; 16], // there needs to be 16 of these
 }
-
-
 
 fn main() {
     println!("Hello, world!");
